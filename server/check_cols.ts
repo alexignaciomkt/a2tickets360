21e7dc01e60b7e108027978eecc3f455fcc37007
Gigante Pneus <mktgigantepneus@gmail.com>
2026-08-25 21:12:52 -0300

    fix: finalize stabilized event publishing and featured flow

--- server/check_cols.ts
+++
@@ -1,8 +0,0 @@
-import { db } from './src/db/index';
-import { sql } from 'drizzle-orm';
-async function run() {
-    const res = await db.execute(sql.raw("SELECT column_name FROM information_schema.columns WHERE table_name = 'organizer_details'"));
-    console.log(res);
-    process.exit(0);
-}
-run();
--- server/check_constraint.ts
+++
@@ -1,8 +0,0 @@
-import { db } from './src/db/index';
-import { sql } from 'drizzle-orm';
-async function run() {
-    const res = await db.execute(sql.raw("SELECT pg_get_constraintdef(oid) FROM pg_constraint WHERE conname = 'purchased_tickets_parent_purchase_id_fkey'"));
-    console.log(res);
-    process.exit(0);
-}
-run();
